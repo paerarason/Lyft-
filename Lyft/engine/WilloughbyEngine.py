472f504cc9aec99be0b6b05cb84b91403f3b4940
your_github_username <paerarason1111@gmail.com>
2022-11-08 01:54:23 +0530

    tested

---
+++ Lyft/engine/WilloughbyEngine.py
@@ -0,0 +1,10 @@
+from engine.engine import Engine
+class WilloughbyEngine:
+    def __init__(self,current_milage,last_service_mileage) :
+        self.last_service_mileage=last_service_mileage
+        self.current_milage=current_milage
+    def needs_service(self):
+        if self.current_milage-self.last_service_mileage >60000 :
+           return True
+        else:
+            return False
